9e63e7951d1df673f641bf143644cd10281945ba
Zac Jones <zacjones91@gmail.com>
2018-12-05 11:14:36 -0600

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import ApplicationViews from "./components/ApplicationViews";
+import Navbar from "./components/nav/NavBar"
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Navbar />
         <ApplicationViews />
       </React.Fragment>
     );
