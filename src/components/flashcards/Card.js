31bb5b6b5747d0f6c932b9b447b867910068914a
Zac Jones <zacjones91@gmail.com>
2018-12-11 10:55:34 -0600

    added css card flip animation

--- src/components/flashcards/Card.js
+++ src/components/flashcards/Card.js
@@ -8,14 +8,17 @@ export default class CardModule extends Component {
   render() {
     console.log(this.props.currentCard);
     return (
-      <div className="card-container">
-        <div className="card">
-          <div className="front">
-            <div className="spanish">{this.props.currentCard.front}</div>
-          </div>
-          <div className="back">
-            <div className="english" />
-            {this.props.currentCard.back}
+      <React.Fragment>
+        <div className="card-container">
+          <div className="card">
+            <div className="front">
+              <div className="spanish">{this.props.currentCard.front}</div>
+            </div>
+            <div className="back">
+              <div className="engrish">
+              {this.props.currentCard.back}
+              </div>
+            </div>
           </div>
         </div>
         <div className="flashcard-button-row">
@@ -30,7 +33,7 @@ export default class CardModule extends Component {
         <div className="flashcard-button-row-bottom">
           <Button
             basic
-            color="gray"
+            color="grey"
             onClick={() => {
               this.props.backToSelection();
             }}
@@ -38,7 +41,7 @@ export default class CardModule extends Component {
             Select Another Quiz
           </Button>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
