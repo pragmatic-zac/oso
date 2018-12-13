093901445766668da1be7ef43851f76cce356f4e
Zac Jones <zacjones91@gmail.com>
2018-12-13 14:05:55 -0600

    add voice select section, flags as icons, cons logging voice ID

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
-import { Header, Container } from "semantic-ui-react";
+import { Header, Container, Message, Grid } from "semantic-ui-react";
+import "./Profile.css";
+import spain from "../../images/spain.png";
+import mexico from "../../images/mexico.png";
 
 export default class UserProfile extends Component {
+  onClick = number => {
+    console.log(number);
+  };
+
   render() {
     // find the logged in user's info
     let currentUserInfo = this.props.users.filter(user => {
@@ -16,6 +23,74 @@ export default class UserProfile extends Component {
           <Header as="h1">Your profile</Header>
           <br />
           <Header as="h3"> {currentUserInfo[0].username}</Header>
+          <Message>
+            <Header as="h5">Select Your Voice</Header>
+            <p className="currentVoice">
+              Current selection: {currentUserInfo[0].voice}
+            </p>
+            <br />
+            <Grid>
+              <br />
+              <Grid.Column width={4}>
+                <div className="voice-option">
+                  <img
+                    src={spain}
+                    alt="spain"
+                    className="image"
+                    id="14"
+                    onClick={() => {
+                      this.onClick(14);
+                    }}
+                  />
+                  <br />
+                  Jorge - Spain
+                </div>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <div className="voice-option">
+                  <img
+                    src={mexico}
+                    alt="mexico"
+                    className="image"
+                    onClick={() => {
+                      this.onClick(15);
+                    }}
+                  />
+                  <br />
+                  Juan - Mexico
+                </div>
+              </Grid.Column>
+
+              <Grid.Column width={4}>
+                <div className="voice-option">
+                  <img
+                    src={mexico}
+                    alt="mexico"
+                    className="image"
+                    onClick={() => {
+                      this.onClick(31);
+                    }}
+                  />
+                  <br />
+                  Paulina - Mexico
+                </div>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <div className="voice-option">
+                  <img
+                    src={spain}
+                    alt="spain"
+                    className="image"
+                    onClick={() => {
+                      this.onClick(29);
+                    }}
+                  />
+                  <br />
+                  Monica - Spain{" "}
+                </div>
+              </Grid.Column>
+            </Grid>
+          </Message>
         </Container>
       </React.Fragment>
     );
